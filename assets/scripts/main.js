a4c6a7609de57f082cc5d2ad4da3e9ac12e9ca09
Caio Cunha <160679697+caiodscunha@users.noreply.github.com>
2026-02-22 19:52:43 -0300

    Add files via upload

---
+++ assets/scripts/main.js
@@ -0,0 +1,49 @@
+let stage = {
+    theme: localStorage.getItem('theme'),
+    references: {
+        logo_img: document.getElementById('brand_logo'),
+        toggleThemeIcon: document.getElementById('toggleTheme'),
+        root: document.documentElement,
+        menuLinks: document.querySelectorAll('.menu__link'),
+    },
+};
+
+stage.references.root.setAttribute('data-theme', stage.theme);
+
+// Initialize icon based on theme
+if (stage.theme === 'lightmode') {
+    stage.references.toggleThemeIcon.classList.remove('bi-moon-stars');
+    stage.references.toggleThemeIcon.classList.add('bi-sun');
+} else {
+    stage.references.toggleThemeIcon.classList.remove('bi-sun');
+    stage.references.toggleThemeIcon.classList.add('bi-moon-stars');
+}
+
+function toggleTheme() {
+    let currTheme = stage.references.root.getAttribute('data-theme');
+
+    if (currTheme === "lightmode") {
+        stage.references.root.setAttribute('data-theme', 'darkmode')
+        localStorage.setItem('theme', 'darkmode');
+        stage.references.toggleThemeIcon.classList.remove('bi-sun');
+        stage.references.toggleThemeIcon.classList.add('bi-moon-stars');
+
+    } else {
+        stage.references.root.setAttribute('data-theme', 'lightmode')
+        localStorage.setItem('theme', 'lightmode');
+        stage.references.toggleThemeIcon.classList.remove('bi-moon-stars');
+        stage.references.toggleThemeIcon.classList.add('bi-sun');
+    }
+}
+
+function toggleActiveAccordion(button) {
+    const accordionItem = button.parentElement;
+    accordionItem.classList.toggle('active');
+}
+
+stage.references.menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        stage.references.menuLinks.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
